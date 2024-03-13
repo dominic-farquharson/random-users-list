3a66fdeaf2aa88870017a6acba1d5ab7a0720142
Dominic Farquharson <farquharsondominic@gmail.com>
2024-03-13 10:41:28 -0400

    add mui to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
-import "@/styles/globals.css";
+import * as React from "react";
 import type { AppProps } from "next/app";
+import { AppCacheProvider } from "@mui/material-nextjs/v13-pagesRouter";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { Inter } from "next/font/google";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const inter = Inter({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const theme = createTheme({
+  typography: {
+    fontFamily: inter.style.fontFamily,
+  },
+});
+
+export default function MyApp(props: AppProps) {
+  const { Component, pageProps } = props;
+  return (
+    <AppCacheProvider {...props}>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </AppCacheProvider>
+  );
 }
