f424beb5e854aa3eee1c9e52400e3e45f32161d9
Dominic Farquharson <farquharsondominic@gmail.com>
2024-03-13 10:42:19 -0400

    add users list

---
+++ src/components/UsersList/UserCard.tsx
@@ -0,0 +1,51 @@
+import { Name } from "@/services/randomUserApi.types";
+import {
+  Card,
+  Button,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+
+
+type UserCardProps = {
+  profileImage: string;
+  name: Name;
+  age: number;
+  city: string;
+  id: string;
+  onClick: () => void
+};
+
+export const UserCard = (props: UserCardProps) => {
+  const fullName = [props.name.title, props.name.first, props.name.last].join(
+    " "
+  );
+
+  return (
+    <Card sx={{ maxWidth: 345, cursor: 'pointer' }} onClick={props.onClick}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image={props.profileImage}
+        title={fullName}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {fullName}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Age: {props.age}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          City: {props.city}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" onClick={props.onClick}>
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
---
+++ src/components/UsersList/UsersList.tsx
@@ -0,0 +1,32 @@
+import { GetUsersResponse } from "@/services/randomUserApi.types";
+import {
+  Grid,
+} from "@mui/material";
+import { UserCard } from "./UserCard";
+import React from "react";
+
+export interface UsersTableProps {
+  users: GetUsersResponse["results"];
+  setUserDetail: React.Dispatch<React.SetStateAction<GetUsersResponse["results"][0] | null>>
+}
+
+export const UsersTable: React.FC<UsersTableProps> = (props) => {
+  return (
+    <Grid container spacing={3}>
+      {props.users.map((el) => {
+        return (
+          <Grid item xs={12} md={6} lg={4} key={el.login.uuid}>
+            <UserCard
+              age={el.dob.age}
+              profileImage={el.picture.large}
+              city={el.location.city}
+              name={el.name}
+              id={el.login.uuid}
+              onClick={() => props.setUserDetail(el)}
+            />
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
+};
